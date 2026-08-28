ab82b43a67942be100f373d3ff75ced9f824a5db
Mohammad Mostafa Dastjerdi <ezmmd83@gmail.com>
2026-08-28 17:43:07 +0330

    added loots for sword

--- src/main/kotlin/ir/mmd/mcdev/moretools/Main.kt
+++ src/main/kotlin/ir/mmd/mcdev/moretools/Main.kt
@@ -1,9 +1,33 @@
 package ir.mmd.mcdev.moretools
 
+import com.mojang.serialization.MapCodec
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents
+import net.minecraft.core.registries.Registries
+import net.minecraft.world.item.enchantment.Enchantments
+import net.minecraft.world.item.enchantment.LevelBasedValue
+import net.minecraft.world.level.storage.loot.BuiltInLootTables
+import net.minecraft.world.level.storage.loot.LootPool
+import net.minecraft.world.level.storage.loot.entries.LootItem
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceWithEnchantedBonusCondition
 
 class Main : ModInitializer {
 	override fun onInitialize() {
 		Items.load()
+		LootTableEvents.MODIFY.register { key, builder, source, provider ->
+			if (source.isBuiltin && key == BuiltInLootTables.ANCIENT_CITY) {
+				builder.withPool(
+					LootPool.lootPool().add(
+						LootItem.lootTableItem(Items.AMETHYST_SWORD)
+							.`when` {
+								LootItemRandomChanceWithEnchantedBonusCondition(
+									0.17f, LevelBasedValue.Linear(0.05f, 0.07f),
+									provider.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE)
+								)
+							}
+					)
+				)
+			}
+		}
 	}
 }
