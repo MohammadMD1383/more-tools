214845f45d05bc151531f7ead8b10f08710e912c
Mohammad Mostafa Dastjerdi <ezmmd83@gmail.com>
2026-08-30 22:05:42 +0330

    added crafting recipes

--- src/client/kotlin/ir/mmd/mcdev/moretools/client/providers/CraftingRecipeProvider.kt
+++ src/client/kotlin/ir/mmd/mcdev/moretools/client/providers/CraftingRecipeProvider.kt
@@ -8,6 +8,7 @@ import net.minecraft.data.recipes.RecipeOutput
 import net.minecraft.data.recipes.RecipeProvider
 import net.minecraft.data.recipes.RecipeProvider.getHasName
 import net.minecraft.world.item.Items
+import net.minecraft.world.level.ItemLike
 import java.util.concurrent.CompletableFuture
 import ir.mmd.mcdev.moretools.Items as MyItems
 
@@ -25,66 +26,204 @@ class CraftingRecipeProvider(
 		}
 	}
 	
+	private fun RecipeProvider.swordCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("X")
+			.pattern("X")
+			.pattern("S")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.pickaxeCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.TOOLS, outcome)
+			.pattern("XXX")
+			.pattern(" S ")
+			.pattern(" S ")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.axeCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.TOOLS, outcome)
+			.pattern("XX")
+			.pattern("XS")
+			.pattern(" S")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.shovelCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.TOOLS, outcome)
+			.pattern("X")
+			.pattern("S")
+			.pattern("S")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.hoeCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.TOOLS, outcome)
+			.pattern("XX")
+			.pattern(" S")
+			.pattern(" S")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.spearCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("  X")
+			.pattern(" S ")
+			.pattern("S  ")
+			.define('X', material)
+			.define('S', Items.STICK)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.allToolsCraftingRecipe(
+		material: ItemLike,
+		sword: ItemLike,
+		pickaxe: ItemLike,
+		axe: ItemLike,
+		shovel: ItemLike,
+		hoe: ItemLike,
+		spear: ItemLike,
+		output: RecipeOutput
+	) {
+		swordCraftingRecipe(material, sword, output)
+		pickaxeCraftingRecipe(material, pickaxe, output)
+		axeCraftingRecipe(material, axe, output)
+		shovelCraftingRecipe(material, shovel, output)
+		hoeCraftingRecipe(material, hoe, output)
+		spearCraftingRecipe(material, spear, output)
+	}
+	
+	private fun RecipeProvider.helmetCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("XXX")
+			.pattern("X X")
+			.define('X', material)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.chestplateCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("X X")
+			.pattern("XXX")
+			.pattern("XXX")
+			.define('X', material)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.leggingsCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("XXX")
+			.pattern("X X")
+			.pattern("X X")
+			.define('X', material)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.bootsCraftingRecipe(
+		material: ItemLike,
+		outcome: ItemLike,
+		output: RecipeOutput
+	) {
+		shaped(RecipeCategory.COMBAT, outcome)
+			.pattern("X X")
+			.pattern("X X")
+			.define('X', material)
+			.unlockedBy(getHasName(material), has(material))
+			.save(output)
+	}
+	
+	private fun RecipeProvider.allArmorCraftingRecipe(
+		material: ItemLike,
+		helmet: ItemLike,
+		chestplate: ItemLike,
+		leggings: ItemLike,
+		boots: ItemLike,
+		output: RecipeOutput
+	) {
+		helmetCraftingRecipe(material, helmet, output)
+		chestplateCraftingRecipe(material, chestplate, output)
+		leggingsCraftingRecipe(material, leggings, output)
+		bootsCraftingRecipe(material, boots, output)
+	}
+	
 	override fun createRecipeProvider(
 		registries: HolderLookup.Provider,
 		output: RecipeOutput
 	) = recipeProvider(registries, output) {
-		val amethystName = getHasName(Items.AMETHYST_SHARD)
-		val amethystItem = has(Items.AMETHYST_SHARD)
-		
-		shaped(RecipeCategory.COMBAT, MyItems.AMETHYST_SWORD)
-			.pattern("A")
-			.pattern("A")
-			.pattern("S")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
-		
-		shaped(RecipeCategory.COMBAT, MyItems.AMETHYST_SPEAR)
-			.pattern("  A")
-			.pattern(" S ")
-			.pattern("S  ")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
-		
-		shaped(RecipeCategory.TOOLS, MyItems.AMETHYST_PICKAXE)
-			.pattern("AAA")
-			.pattern(" S ")
-			.pattern(" S ")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
-		
-		shaped(RecipeCategory.TOOLS, MyItems.AMETHYST_AXE)
-			.pattern("AA")
-			.pattern("AS")
-			.pattern(" S")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
-		
-		shaped(RecipeCategory.TOOLS, MyItems.AMETHYST_SHOVEL)
-			.pattern("A")
-			.pattern("S")
-			.pattern("S")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
-		
-		shaped(RecipeCategory.TOOLS, MyItems.AMETHYST_HOE)
-			.pattern("AA")
-			.pattern(" S")
-			.pattern(" S")
-			.define('A', Items.AMETHYST_SHARD)
-			.define('S', Items.STICK)
-			.unlockedBy(amethystName, amethystItem)
-			.save(output)
+		allToolsCraftingRecipe(
+			Items.AMETHYST_SHARD,
+			MyItems.AMETHYST_SWORD,
+			MyItems.AMETHYST_PICKAXE,
+			MyItems.AMETHYST_AXE,
+			MyItems.AMETHYST_SHOVEL,
+			MyItems.AMETHYST_HOE,
+			MyItems.AMETHYST_SPEAR,
+			output
+		)
+		allArmorCraftingRecipe(
+			Items.AMETHYST_SHARD,
+			MyItems.AMETHYST_HELMET,
+			MyItems.AMETHYST_CHESTPLATE,
+			MyItems.AMETHYST_LEGGINGS,
+			MyItems.AMETHYST_BOOTS,
+			output
+		)
 	}
 	
 	override fun getName() = "More Tools Recipes"
--- src/main/kotlin/ir/mmd/mcdev/moretools/ArmorMaterials.kt
+++ src/main/kotlin/ir/mmd/mcdev/moretools/ArmorMaterials.kt
@@ -5,5 +5,5 @@ import net.minecraft.world.item.equipment.ArmorMaterial
 import net.minecraft.world.item.equipment.ArmorMaterials.makeDefense
 
 object ArmorMaterials {
-	@JvmStatic val AMETHYST = ArmorMaterial(19, makeDefense(2, 5, 6, 2, 5), 30, SoundEvents.ARMOR_EQUIP_DIAMOND, 0f, 0f, ItemTags.AMETHYST_TOOL_MATERIALS, EquipmentAssets.AMETHYST)
+	@JvmStatic val AMETHYST = ArmorMaterial(19, makeDefense(2, 5, 6, 2, 5), 28, SoundEvents.ARMOR_EQUIP_DIAMOND, 0f, 0f, ItemTags.AMETHYST_TOOL_MATERIALS, EquipmentAssets.AMETHYST)
 }
